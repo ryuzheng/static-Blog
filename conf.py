24c671988f115384e1c39508151c057a76be5ab7
Zexin Zheng <5512323+ryuzheng@users.noreply.github.com>
2019-12-18 23:58:04 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,14 +14,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "郑泽鑫的博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "ryuzheng"
+email = "ryu@zhengzexin.com"
+author_homepage = "https://zhengzexin.com"
+description = "一个生信工作者的独立博客"
+key_words = ['生物信息学', 'Geek', 'Python', '独立博客']
 language = 'zh-CN'
 external_links = [
     {
