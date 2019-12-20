3f76bd3d42b69d9238c3d55f1b1f374f27fed483
ryuzheng <ryuzhengzexin@gmail.com>
2019-12-20 21:43:19 +0800

    1. 添加代码

--- conf.py
+++ conf.py
@@ -59,4 +59,15 @@ head_addon = r'''
 
 footer_addon = ''
 
-body_addon = ''
+body_addon = r'''
+<script>
+    if(window.location.hash){
+        var checkExist = setInterval(function() {
+           if ($(window.location.hash).length) {
+              $('html, body').animate({scrollTop: $(window.location.hash).offset().top-90}, 1000);
+              clearInterval(checkExist);
+           }
+        }, 100);
+    }
+</script>
+'''
